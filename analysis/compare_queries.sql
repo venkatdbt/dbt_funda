13852f479554d647c902de3bd59455301b8d9c18
venkat b <venkatbommepalli@gmail.com>
2024-12-09 11:29:10 +0000

    added comparator

--- analysis/compare_queries.sql
+++ analysis/compare_queries.sql
@@ -1,8 +1,10 @@
 {% set old_etl_relation = ref("customer_orders2") %}
 
 {% set dbt_relation = ref("fct_customer_orders2") %}
+
 {{
     audit_helper.compare_relations(
-        a_relation=old_etl_relation, b_relation=dbt_relation, primary_key="order_id"
+        a_relation=old_etl_relation, b_relation=dbt_relation, 
+        primary_key="order_id"
     )
 }}
